278dadeb85593cb1107370eae1e3b2302404eeca
idlers <778826356@qq.com>
2023-06-07 16:47:22 +0800

    引入静态资源

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-// import './assets/main.css'
 import {createApp} from 'vue'
 import {createPinia} from "pinia";
+
 import App from './App.vue'
 import router from './router'
 
-import {getCategory} from "@/apis/testApi";
+// 引入初始化的样式文件
+import '@/styles/common.scss'
 
+// 测试接口函数
+import {getCategory} from "@/apis/testApi";
 getCategory().then(res => {
     console.log(res)
 })
